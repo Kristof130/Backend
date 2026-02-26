4ea9158cd8ca3c2ef3f92dc0d9de900b26504cb5
Kristof130 <pappkristof2006@gmail.com>
2026-02-26 08:55:04 +0100

    adatbazishozzaadas1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
+using Microsoft.EntityFrameworkCore;
 using GameStore.Data;
 using GameStore.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var connString = builder.Configuration.GetConnectionString("GameStore");
-builder.Services.AddSqlite<GameStoreContext>(connString); 
+builder.Services.AddDbContext<GokartkolcsonzoContext>(options =>
+    options.UseMySql(connString, ServerVersion.AutoDetect(connString)));
 var app = builder.Build();
 
 app.MapGamesEndpoints(); 
