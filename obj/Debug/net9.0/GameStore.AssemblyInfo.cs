98fd07067b233604f8bf43b211e08bca174eec5f
Kristof130 <pappkristof2006@gmail.com>
2026-02-26 08:38:47 +0100

    migrationsdelete

--- obj/Debug/net9.0/GameStore.AssemblyInfo.cs
+++ obj/Debug/net9.0/GameStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1ae83d3081a304fd556fb2ab876bfaffb65a39e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b8ea7431dee12de4a25648bc76b0425132e14ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
