a01a598237d8a2bd2b841a72d3fe7d7dacb20ad3
Kristof130 <pappkristof2006@gmail.com>
2026-03-03 10:35:15 +0100

    recommit

---
+++ Entities/Gokartok.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace GameStore.Entities;
+
+public class Gokartok
+{
+public int Id { get; set; }
+public  string Gyarto { get; set; }
+public string Tipus { get; set; }
+public  DateOnly Evjarat { get; set; }
+public string VazTipusa { get; set; }
+public int Tomeg { get; set; }
+
+public int Hossz { get; set; }
+
+public int Szelesseg { get; set; }
+
+public int Tengelytav { get; set; }
+
+public string MotorTipusa { get; set; }
+
+public string HajtasModja { get; set; }
+
+public string ValtoTipusa { get; set; }
+
+public bool Foglalt_e { get; set; }
+
+public int NapiAr { get; set; }
+}
